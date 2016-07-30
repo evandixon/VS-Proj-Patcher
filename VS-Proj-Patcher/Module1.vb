0eeb73011b6c4e9518ecc58773961a499a03040e
Evan Dixon <evanldixon@live.com>
2016-07-30 14:53:55 -0500

    Initial commit

---
+++ VS-Proj-Patcher/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.Build.BuildEngine
+Module Module1
+
+    Sub Main()
+        Dim args = Environment.GetCommandLineArgs
+        Dim FullProjectPath As String = args(1)
+        Dim eng As New Engine()
+        Dim proj As New Project(eng)
+        proj.Load(FullProjectPath)
+        proj.SetProperty("NoWin32Manifest", "true")
+        proj.Save(FullProjectPath)
+    End Sub
+
+End Module
---
+++ VS-Proj-Patcher/Readme.md
@@ -0,0 +1,3 @@
+﻿# Vs-Proj-Patcher
+
+Currently, Vs-Proj-Patcher takes a project file, and sets the NoWin32Manifest property to true, for use with automated ClickOnce Deployments
